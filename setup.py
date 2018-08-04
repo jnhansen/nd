a279a8aee67e66791f7cfd059bcc9b19727dce54
Johannes Hansen <johannes.hansen@ed.ac.uk>
2018-08-04 16:15:47 +0200

    add dependencies

--- setup.py
+++ setup.py
@@ -3,10 +3,23 @@ from Cython.Distutils import build_ext
 import numpy
 
 
-ext_modules = [Extension("geo._change", ["geo/_change.pyx"],)]
+ext_modules = [Extension("geo._omnibus", ["geo/_omnibus.pyx"],)]
 
 setup(name='geotools',
       packages=find_packages(),
       cmdclass={'build_ext': build_ext},
       ext_modules=ext_modules,
-      include_dirs=[numpy.get_include()])
+      include_dirs=[numpy.get_include()],
+      install_requires=[
+        "numpy",
+        "scipy",
+        "xarray",
+        "dask",
+        "cython",
+        "lxml",
+        "gdal",
+        "pandas",
+        "python-dateutil",
+        "scikit-image",
+        "matplotlib"
+      ])
