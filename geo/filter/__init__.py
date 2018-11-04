8be59c7d1c86a2db6fde3c2341dda8ea9e0e337c
Johannes Hansen <johannes.hansen@ed.ac.uk>
2018-11-04 09:10:49 +0000

    deal with <3D cases

--- geo/filter/__init__.py
+++ geo/filter/__init__.py
@@ -71,27 +71,35 @@ def nlmeans(ds, r, sigma, h, f=1, **kwargs):
     f : int
 
     """
-    for k in ['lat', 'lon', 'time']:
-        if k not in r:
-            r[k] = 0
+    orig_dims = tuple(ds.dims)
     dims = tuple(r.keys())
+
     r_ = np.array(list(r.values()), dtype=np.uint32)
     f_ = np.array([f if _ > 0 else 0 for _ in r_], dtype=np.uint32)
 
     variables = get_vars_for_dims(ds, dims)
     other_variables = get_vars_for_dims(ds, dims, invert=True)
-    ordered_dims = dims + ('variable',) + tuple(set(ds.dims) - set(dims))
+
+    # Use ds.coords here to preserve coordinates that are not dimensions.
+    # This is important for lower-dimensional slices.
+    ordered_dims = dims + tuple(set(orig_dims) - set(dims)) + ('variable',)
 
     # convert to DataArray
     da_ordered = ds[variables].to_array().transpose(*ordered_dims)
     da_filtered = da_ordered.copy()
 
-    arr = da_ordered.values
-    output = da_filtered.values
+    # Pad dimensions to 3D (4D with variables)
+    # Extra dimensions are prepended
+    arr = np.array(da_ordered.values, ndmin=4, copy=False)
+    output = np.array(da_filtered.values, ndmin=4, copy=False)
+    pad = np.zeros(3 - len(r_), dtype=r_.dtype)
+    r_ = np.concatenate([pad, r_])
+    f_ = np.concatenate([pad, f_])
 
     _pixelwise_nlmeans_3d(arr, output, r_, f_, sigma, h)
 
-    result = expand_variables(da_filtered)
+    result = expand_variables(da_filtered).transpose(*orig_dims)
+
     for v in other_variables:
         result[v] = ds[v]
 
