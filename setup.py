7a67ca3d981358be246b7e88add8f7dce9d1b87a
Johannes Hansen <johannes.hansen@ed.ac.uk>
2018-11-05 10:20:13 +0000

    fix setup script

--- setup.py
+++ setup.py
@@ -1,56 +1,67 @@
-from setuptools import setup, find_packages, Extension
+from setuptools import setup, Extension
 import subprocess
-from Cython.Distutils import build_ext
 import numpy
 import cython_gsl
+
+try:
+    from Cython.Distutils import build_ext
+    from Cython.Build import cythonize
+except ImportError:
+    use_cython = False
+else:
+    use_cython = True
+
+ext = '.pyx' if use_cython else '.c'
 
 # from Cython.Compiler.Options import get_directive_defaults
 # directive_defaults = get_directive_defaults()
 # directive_defaults['linetrace'] = True
 # directive_defaults['binding'] = True
 
-ext_modules = [
-    Extension("geo.change._omnibus", ["geo/change/_omnibus.pyx"],
+extensions = [
+    Extension("geo.change._omnibus", ["geo/change/_omnibus" + ext],
               libraries=cython_gsl.get_libraries(),
               library_dirs=[cython_gsl.get_library_dir()],
               include_dirs=['.', cython_gsl.get_cython_include_dir()],
               extra_compile_args=['-O3', '-fopenmp'],
               extra_link_args=['-fopenmp'],
               ),
-    Extension("geo.filter._nlmeans", ["geo/filter/_nlmeans.pyx"],
+    Extension("geo.filter._nlmeans", ["geo/filter/_nlmeans" + ext],
               extra_compile_args=['-O3', '-fopenmp'],
               extra_link_args=['-fopenmp'],
               ),
-    Extension("geo._warp", ["geo/_warp.pyx"]),
+    Extension("geo._warp", ["geo/_warp" + ext]),
 ]
+
+if use_cython:
+    extensions = cythonize(extensions)
+    cmdclass = {'build_ext': build_ext}
+else:
+    cmdclass = {}
+
 
 gdal_version = subprocess.check_output(
     ['gdal-config', '--version']).decode('utf-8').strip('\n')
 gdal_version_range = [gdal_version + '.0', gdal_version + '.999']
 
 setup(
-    # name='geo',
-    # packages=find_packages(),
-    cmdclass={'build_ext': build_ext},
-    ext_modules=ext_modules,
+    cmdclass=cmdclass,
+    ext_modules=extensions,
     include_dirs=[
         numpy.get_include(),
         cython_gsl.get_include()
     ],
     install_requires=[
-        # "gcc",
         "numpy",
         "scipy",
         "xarray",
-        "dask",
-        "cython",
+        "dask[dataframe]",
         "lxml",
         "pygdal>={},<={}".format(*gdal_version_range),
         "pandas",
         "python-dateutil",
         "matplotlib",
         "affine",
-        "CythonGSL",
         "opencv-python"
     ]
 )
