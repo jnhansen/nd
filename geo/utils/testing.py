a75002fe85333f701e080f6b9b81a18f9335da2a
Johannes Hansen <johannes.hansen@ed.ac.uk>
2018-11-05 14:50:32 +0000

    add equal_list_of_dicts

--- geo/utils/testing.py
+++ geo/utils/testing.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import xarray as xr
+import json
 
 
 def generate_test_dataset(nlat=20, nlon=20, ntime=10,
@@ -20,3 +21,46 @@ def generate_test_dataset(nlat=20, nlon=20, ntime=10,
         ds[v] = (('lat', 'lon', 'time'),
                  np.random.normal(m, sigma, (nlat, nlon, ntime)))
     return ds
+
+
+def equal_list_of_dicts(obj1, obj2, exclude=[]):
+    """Check whether two lists of dictionaries are equal, independent of the
+    order within the list.
+    Parameters
+    ----------
+    obj1 : list of dict
+        First object to compare
+    obj2 : list of dict
+        Second object to compare
+    exclude : list of str, optional
+        A list of keys that are to be excluded when deciding whether the lists
+        of dictionaries are equal (default: []).
+    Returns
+    -------
+    bool
+        True if the two lists contain the same dictionaries, False otherwise.
+    """
+    for key in exclude:
+        for i in obj1:
+            del i[key]
+        for i in obj2:
+            del i[key]
+    serial1 = [json.dumps(_, sort_keys=True) for _ in obj1]
+    serial2 = [json.dumps(_, sort_keys=True) for _ in obj2]
+
+    return set(serial1) == set(serial2)
+
+
+# def generate_test_latlon_grid(shape):
+#     y, x = np.meshgrid(np.arange(shape[1]),
+#                        np.arange(shape[0]),
+#                        copy=False)
+#     xy = np.stack((x, y), axis=-1).reshape((-1, 2))
+#     p = PolynomialFeatures(degree=1)
+#     xy_poly = p.fit_transform(xy)
+#     n_coefs = xy_poly.shape[1]
+#     coefs_lat = np.random.normal(size=n_coefs)
+#     coefs_lon = np.random.normal(size=n_coefs)
+#     lat = (xy_poly * coefs_lat).sum(axis=1).reshape(shape)
+#     lon = (xy_poly * coefs_lon).sum(axis=1).reshape(shape)
+#     return lat, lon
