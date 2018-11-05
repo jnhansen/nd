94c3c7d9c83a032d64ebfd10c26032974f2fd334
Johannes Hansen <johannes.hansen@ed.ac.uk>
2018-11-05 16:25:50 +0000

    test parallel() decorator

--- geo/utils/tests/test_utils.py
+++ geo/utils/tests/test_utils.py
@@ -1,7 +1,10 @@
 import numpy as np
 from numpy.testing import assert_equal
+from xarray.testing import assert_equal as xr_assert_equal
+from xarray.testing import assert_identical as xr_assert_identical
 from geo import utils
-from geo.utils import testing
+from geo.utils.testing import equal_list_of_dicts, generate_test_dataset
+import time
 
 
 def test_str2date():
@@ -16,7 +19,7 @@ def test_dict_product():
               {'a': 2, 'b': 6},
               {'a': 3, 'b': 5},
               {'a': 3, 'b': 6}]
-    assert testing.equal_list_of_dicts(list(utils.dict_product(d)), result)
+    assert equal_list_of_dicts(list(utils.dict_product(d)), result)
 
 
 def test_chunks():
@@ -24,3 +27,24 @@ def test_chunks():
     n = 15
     for i, c in enumerate(utils.chunks(ll, n)):
         assert_equal(c, ll[i*n:(i+1)*n])
+
+
+def _parallel_fn(ds):
+    # Simulate some work with O(N) ...
+    size = ds.count().to_array().values.sum()
+    time.sleep(size / 10000.)
+    return ds + 1
+
+
+def test_parallel():
+    ds = generate_test_dataset()
+    t = time.time()
+    result_serial = _parallel_fn(ds)
+    serial_time = time.time() - t
+    t = time.time()
+    result_parallel = utils.parallel(_parallel_fn, chunks=4)(ds)
+    parallel_time = time.time() - t
+    # Assert that the results are identical
+    xr_assert_identical(result_serial, result_parallel)
+    # Assert that the parallel execution was more than three times faster
+    assert np.ceil(serial_time / parallel_time) == 4
