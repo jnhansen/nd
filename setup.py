dc275c478c8b1257d8b4be7f9a6a0e39893b2cc4
Johannes Hansen <johannes.hansen@ed.ac.uk>
2018-11-24 17:54:05 +0000

    check if mock install

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, Extension
 import subprocess
+import os
+
+mock_install = os.environ.get('READTHEDOCS') == 'True'
+
+if not mock_install:
+    import numpy
 
 try:
-    import numpy
     import cython_gsl
     from Cython.Distutils import build_ext
     from Cython.Build import cythonize
@@ -27,45 +32,49 @@ omnibus_libraries = []
 omnibus_library_dirs = []
 omnibus_include_dirs = ['.']
 
-if use_cython:
+if use_cython and not mock_install:
     omnibus_libraries.extend(cython_gsl.get_libraries())
     omnibus_library_dirs.append(cython_gsl.get_library_dir())
     omnibus_include_dirs.append(cython_gsl.get_cython_include_dir())
 
-extensions = [
-    Extension("nd.change._omnibus", ["nd/change/_omnibus" + ext],
-              libraries=omnibus_libraries,
-              library_dirs=omnibus_library_dirs,
-              include_dirs=omnibus_include_dirs,
-              extra_compile_args=['-O3', '-fopenmp'],
-              extra_link_args=['-fopenmp'],
-              ),
-    Extension("nd.filter._nlmeans", ["nd/filter/_nlmeans" + ext],
-              extra_compile_args=['-O3', '-fopenmp'],
-              extra_link_args=['-fopenmp'],
-              ),
-    Extension("nd._warp", ["nd/_warp" + ext]),
-]
 
-if use_cython:
-    extensions = cythonize(extensions)
-    cmdclass = {'build_ext': build_ext}
+cmdclass = {}
+
+if mock_install:
+    extensions = []
 else:
-    cmdclass = {}
+    extensions = [
+        Extension("nd.change._omnibus", ["nd/change/_omnibus" + ext],
+                  libraries=omnibus_libraries,
+                  library_dirs=omnibus_library_dirs,
+                  include_dirs=omnibus_include_dirs,
+                  extra_compile_args=['-O3', '-fopenmp'],
+                  extra_link_args=['-fopenmp'],
+                  ),
+        Extension("nd.filter._nlmeans", ["nd/filter/_nlmeans" + ext],
+                  extra_compile_args=['-O3', '-fopenmp'],
+                  extra_link_args=['-fopenmp'],
+                  ),
+        Extension("nd._warp", ["nd/_warp" + ext]),
+    ]
 
-try:
+    if use_cython:
+        extensions = cythonize(extensions)
+        cmdclass = {'build_ext': build_ext}
+
+if mock_install:
+    gdal_version_range = [0, 99]
+else:
     gdal_version = subprocess.check_output(
         ['gdal-config', '--version']).decode('utf-8').strip('\n')
     gdal_version_range = [gdal_version + '.0', gdal_version + '.999']
-except FileNotFoundError:
-    # gdal is not installed.
-    # Assume this is a mock install.
-    gdal_version_range = [0, 99]
 
 include_dirs = []
-if use_cython:
+if not mock_install:
     include_dirs.append(numpy.get_include())
-    include_dirs.append(cython_gsl.get_include())
+    if use_cython:
+        include_dirs.append(cython_gsl.get_include())
+
 setup(
     cmdclass=cmdclass,
     ext_modules=extensions,
