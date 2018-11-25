d0e335045e3f7f83b82e4dfed38b67e15d7b9705
Johannes Hansen <johannes.hansen@ed.ac.uk>
2018-11-25 15:13:51 +0100

    fix module path

--- doc/conf.py
+++ doc/conf.py
@@ -19,7 +19,9 @@ try:
 except ImportError:
     nd_path = os.path.abspath('..')
 else:
-    nd_path = os.path.abspath(nd.__file__)
+    nd_path = os.path.abspath(
+        os.path.join(os.path.dirname(nd.__file__), '..')
+    )
 sys.path.insert(0, nd_path)
 
 # from github_links import make_linkcode_resolve
@@ -109,7 +111,9 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'logo_only': True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
