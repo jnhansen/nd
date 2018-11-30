870662e59da1101676a1228b9ff0c7a17738fca2
Johannes Hansen <johannes.hansen@ed.ac.uk>
2018-11-30 14:22:37 +0000

    document expand_kernel

--- nd/filters/__init__.py
+++ nd/filters/__init__.py
@@ -1,8 +1,13 @@
 """
+The main use of image filters is for noise reduction.
+This module implements several such filters, all of which are designed to work
+in an arbitrary number of dimensions.
+
 .. document private functions
 .. autofunction:: _expand_kernel
 """
-from .nlmeans_ import nlmeans
-from .convolve_ import convolve, boxcar, gaussian
 
-__all__ = ['convolve', 'boxcar', 'nlmeans', 'gaussian']
+from .nlmeans_ import nlmeans
+from .convolve_ import convolve, boxcar, gaussian, _expand_kernel
+
+__all__ = ['convolve', 'boxcar', 'nlmeans', 'gaussian', '_expand_kernel']
