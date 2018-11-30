598f1e1f50cbbe8c37041d9ac4466041a378e825
Johannes Hansen <johannes.hansen@ed.ac.uk>
2018-11-30 14:24:21 +0000

    document _cluster()

--- nd/classify/__init__.py
+++ nd/classify/__init__.py
@@ -1,3 +1,8 @@
-from .classification_ import cluster, norm_by_cluster
+"""
+.. document private functions
+.. autofunction:: _cluster
+"""
 
-__all__ = ['cluster', 'norm_by_cluster']
+from .classification_ import cluster, norm_by_cluster, _cluster
+
+__all__ = ['_cluster', 'cluster', 'norm_by_cluster']
