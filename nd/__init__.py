8389ad84938e55001378e670928bcd1718e91a83
Johannes Hansen <johannes.hansen@ed.ac.uk>
2018-12-08 01:24:46 +0000

    import h5netcdf as first module

--- nd/__init__.py
+++ nd/__init__.py
@@ -1,1 +1,5 @@
+# Strangely, importing h5netcdf must be imported at this point
+# for the h5netcdf backend to work properly on Travis (and Ubuntu)
+import h5netcdf
+
 __all__ = ['change', 'classify', 'io', 'visualize', 'filters', 'utils']
