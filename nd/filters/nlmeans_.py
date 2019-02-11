b484cb92680183ac97ec3a3f9ebe898ac2a03f0d
Johannes Hansen <johannes.hansen@ed.ac.uk>
2019-02-11 09:41:36 +0000

    add default dimensions

--- nd/filters/nlmeans_.py
+++ nd/filters/nlmeans_.py
@@ -25,7 +25,7 @@ class NLMeansFilter(Filter):
 
     per_variable = False
 
-    def __init__(self, dims, r=1, sigma=1, h=1, f=1):
+    def __init__(self, dims=('y', 'x'), r=1, sigma=1, h=1, f=1):
         if isinstance(r, (int, float)):
             r = [r] * len(dims)
         self.dims = tuple(dims)
