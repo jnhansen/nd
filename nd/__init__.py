ebbf4c68bc3b74dad3ce9729c8c3d5590a347a07
Johannes Hansen <johannes.hansen@ed.ac.uk>
2019-02-11 09:37:18 +0000

    make common functions available at module level

--- nd/__init__.py
+++ nd/__init__.py
@@ -1,8 +1,17 @@
-# Strangely, importing h5netcdf must be imported at this point
+# Strangely, h5netcdf must be imported at this point
 # for the h5netcdf backend to work properly on Travis (and Ubuntu)
 try:
     import h5netcdf
 except Exception:
     pass
 
-__all__ = ['change', 'classify', 'io', 'visualize', 'filters', 'utils']
+from xarray import Dataset, DataArray
+from .io import open_dataset
+from .visualize import to_rgb, write_video
+
+__all__ = ['change', 'classify', 'io', 'visualize', 'filters', 'utils', 'warp',
+           # Non-modules:
+           'open_dataset',
+           'to_rgb',
+           'write_video'
+           ]
