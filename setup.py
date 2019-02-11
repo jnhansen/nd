73b73b6fe562c045920a04467b9e256f8922fd9d
Johannes Hansen <johannes.hansen@ed.ac.uk>
2019-02-11 10:01:59 +0000

    add scikit-image dependency

--- setup.py
+++ setup.py
@@ -55,7 +55,6 @@ extensions = [
               ),
     # Extension("nd.filters._correlation", ["nd/filters/_correlation" + ext]),
     Extension("nd.warp._warp", ["nd/warp/_warp" + ext]),
-    Extension("nd.change._bayes", ["nd/change/_bayes" + ext]),
 ]
 
 if use_cython:
