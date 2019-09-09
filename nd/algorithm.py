0633a74e44243fb0b8efae890db26fb9e630f40d
Johannes Hansen <johannes.hansen@ed.ac.uk>
2019-09-09 13:13:24 +0100

    add tests for abstract class Algorithm

--- nd/algorithm.py
+++ nd/algorithm.py
@@ -1,7 +1,7 @@
 """
 This module contains the abstract base class Algorithm.
 """
-from abc import ABC, abstractmethod
+from abc import ABC, abstractmethod, ABCMeta
 import inspect
 from types import CodeType
 from collections import OrderedDict
@@ -9,6 +9,7 @@ from .utils import parallel
 
 
 class Algorithm(ABC):
+    __metaclass__ = ABCMeta
 
     @abstractmethod
     def apply(self, ds):
@@ -35,6 +36,7 @@ def extract_arguments(fn, args, kwargs):
     parameters.update(OrderedDict(inspect.signature(_).parameters))
     new_sig = sig.replace(parameters=tuple(parameters.values()))
     bound = new_sig.bind(*args, **kwargs)
+    bound.apply_defaults()
     return bound.arguments
 
 
@@ -89,8 +91,7 @@ def wrap_algorithm(algo, name=None):
 
     # Override docstring
     link = ':class:`{}.{}`'.format(algo.__module__, algo.__name__)
-    _wrapper.__doc__ = """Wrapper for {}.
-    """.format(link) + algo.__doc__
+    _wrapper.__doc__ = "Wrapper for {}.\n".format(link) + algo.__doc__
 
     # Override signature
     sig_init = inspect.signature(algo.__init__)
