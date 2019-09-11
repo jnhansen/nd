05c8f936ee7e2893f8d9e43ca3ec88d44a64d66f
Johannes Hansen <johannes.hansen@ed.ac.uk>
2019-09-11 16:32:06 +0100

    test values produced by rasterize

--- nd/tests/test_vector.py
+++ nd/tests/test_vector.py
@@ -3,10 +3,12 @@ from nd.testing import (generate_test_dataset, generate_test_geodataframe,
                         assert_equal_crs)
 from nd import vector
 from nd import warp
-from numpy.testing import assert_equal
+from numpy.testing import assert_equal, assert_allclose
 from geopandas.testing import assert_geodataframe_equal
 import geopandas as gpd
 import numpy as np
+import rasterio
+from scipy import ndimage
 
 
 def test_rasterize_no_side_effects():
@@ -23,7 +25,7 @@ def test_rasterize_no_side_effects():
 
 def test_rasterize(tmpdir):
     path = str(tmpdir.join('polygons.shp'))
-    ds = generate_test_dataset()
+    ds = generate_test_dataset(dims=dict(x=100, y=100, time=5))
     df = generate_test_geodataframe()
     schema = gpd.io.file.infer_schema(df)
     schema['properties']['date'] = 'date'
@@ -53,6 +55,27 @@ def test_rasterize(tmpdir):
         warp.get_transform(raster),
         warp.get_transform(ds)
     )
+
+    # Check raster content
+    shape = (ds.dims['y'], ds.dims['x'])
+    transform = warp.get_transform(ds)
+    for i, row in df.iterrows():
+        poly = row['geometry']
+        mask = rasterio.features.rasterize(
+            [poly], out_shape=shape, transform=transform
+        )
+        # Erode mask to avoid edge effects
+        mask = ndimage.morphology.binary_erosion(mask) == 1
+
+        for v in raster.data_vars:
+            if 'legend' in raster[v].attrs:
+                expected = sorted(raster[v].attrs['legend'],
+                                  key=lambda x: x[1] == str(row[v]))[-1][0]
+            else:
+                expected = row[v]
+            values = raster[v].isel(time=0).values
+            values[mask]
+            assert_allclose(values[mask], expected)
 
 
 @pytest.mark.parametrize('columns', [
