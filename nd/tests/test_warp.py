dfcea18a6fe02695ba88aa8650a8c48c4ea1ea9c
Johannes Hansen <johannes.hansen@ed.ac.uk>
2019-09-11 14:19:44 +0100

    test reprojection with target

--- nd/tests/test_warp.py
+++ nd/tests/test_warp.py
@@ -6,9 +6,8 @@ from nd.warp import (Reprojection, Resample, Alignment, get_bounds,
                      get_dims, _reproject)
 from nd.io import open_dataset, to_netcdf
 from nd.testing import (generate_test_dataset, generate_test_dataarray,
-                        assert_equal_crs, assert_equal_dict)
+                        assert_equal_crs)
 import numpy as np
-import xarray as xr
 from numpy.testing import (assert_equal, assert_almost_equal, assert_raises,
                            assert_raises_regex)
 from xarray.testing import assert_equal as xr_assert_equal
@@ -95,10 +94,56 @@ def test_reprojection_failure():
     extent = get_extent(ds)
     with assert_raises_regex(
             ValueError, ".* must also specify the `width` and `height`.*"):
-        proj = Reprojection(crs=epsg4326, transform=transform)
+        _ = Reprojection(crs=epsg4326, transform=transform)
     with assert_raises_regex(
             ValueError, "Need to provide either `width` and `height` .*"):
-        proj = Reprojection(crs=epsg4326, extent=extent)
+        _ = Reprojection(crs=epsg4326, extent=extent)
+
+
+@pytest.mark.parametrize('generator', [
+    generate_test_dataset,
+    generate_test_dataarray
+])
+def test_reprojection_with_target(generator):
+    src_crs = epsg4326
+    dst_crs = sinusoidal
+    ds = generator(crs=src_crs)
+    src_bounds = get_bounds(ds)
+    dst_bounds_latlon = BoundingBox(
+        left=src_bounds.left - 1,
+        bottom=src_bounds.bottom - 1,
+        right=src_bounds.right + 1,
+        top=src_bounds.top + 1,
+    )
+    dst_bounds = BoundingBox(*rasterio.warp.transform_bounds(
+        src_crs, dst_crs, **dst_bounds_latlon._asdict()
+    ))
+    dst_width, dst_height = 35, 21
+    resx = (dst_bounds.right - dst_bounds.left) / (dst_width - 1)
+    resy = (dst_bounds.bottom - dst_bounds.top) / (dst_height - 1)
+    res = (abs(resx), abs(resy))
+    xoff = dst_bounds.left
+    yoff = dst_bounds.top
+    dst_transform = Affine(resx, 0, xoff, 0, resy, yoff)
+
+    target = generator(dims={'x': dst_width, 'y': dst_height, 'time': 1},
+                       extent=dst_bounds, crs=dst_crs)
+
+    projected = [
+        Reprojection(crs=dst_crs, transform=dst_transform,
+                     width=dst_width, height=dst_height).apply(ds),
+        Reprojection(crs=dst_crs, extent=dst_bounds,
+                     res=res).apply(ds),
+        Reprojection(crs=dst_crs, extent=dst_bounds,
+                     width=dst_width, height=dst_height).apply(ds),
+        Reprojection(target=target).apply(ds),
+    ]
+    for proj in projected[1:]:
+        xr_assert_equal(proj, projected[0])
+        assert_almost_equal(get_resolution(proj), res)
+        assert_almost_equal(get_bounds(proj), dst_bounds)
+        assert_almost_equal(get_transform(proj), dst_transform)
+        assert_equal_crs(get_crs(proj), dst_crs)
 
 
 @pytest.mark.parametrize('name,kwargs', ds_params)
@@ -376,7 +421,7 @@ def test_reproject_no_hidden_effects():
     dst_crs = sinusoidal
     ds = generate_test_dataset(crs=src_crs)
     ds_copy = ds.copy(deep=True)
-    projected = _reproject(ds_copy, dst_crs=dst_crs)
+    _ = _reproject(ds_copy, dst_crs=dst_crs)
     xr_assert_identical(ds, ds_copy)
 
 
@@ -414,7 +459,7 @@ def test_reproject(generator):
         _reproject(ds, dst_crs=dst_crs, extent=dst_bounds,
                    res=res),
         _reproject(ds, dst_crs=dst_crs, extent=dst_bounds,
-                   width=dst_width, height=dst_height)
+                   width=dst_width, height=dst_height),
     ]
     for proj in projected[1:]:
         xr_assert_equal(proj, projected[0])
