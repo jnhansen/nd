fix broadcasting in classifier

nd/classify.py
@@ -1,14 +1,13 @@
-import xarray as xr
 try:
     from sklearn.cluster import MiniBatchKMeans
     from sklearn import preprocessing
 except (ImportError, ModuleNotFoundError):
     raise ImportError('scikit-learn is required for this module.')
 
+import xarray as xr
 import numpy as np
 from .filters import BoxcarFilter
-from .warp import nrows, ncols
-from .utils import get_vars_for_dims
+from . import utils
 
 
 __all__ = ['Classifier', '_cluster', '_cluster_smooth', 'cluster',
@@ -153,11 +152,52 @@ def _clustermean(ds, labels):
 
 
 def _build_X(ds, feature_dims=[]):
-    variables = get_vars_for_dims(ds, ('y', 'x'))
+    data_dims = tuple([d for d in ds.coords if d not in feature_dims])
+    variables = utils.get_vars_for_dims(ds, data_dims)
     features = tuple(feature_dims) + ('variable',)
     data = ds[variables].to_array().stack(feature=features).transpose(
-        'y', 'x', 'feature', transpose_coords=True).values
+        *data_dims, 'feature', transpose_coords=True).values
     return data.reshape((-1, data.shape[-1]))
+
+
+def _get_data_dims(ds, feature_dims=[]):
+    data_dims = tuple([d for d in ds.coords if d not in feature_dims])
+    return data_dims
+
+
+def _get_data_shape(ds, feature_dims=[]):
+    data_dims = _get_data_dims(ds, feature_dims=feature_dims)
+    shape = tuple([len(ds.coords[d]) for d in data_dims])
+    return shape
+
+
+def _broadcast_array(arr, shape):
+    matching = list(shape)
+    new_shape = [1] * len(shape)
+    for dim in arr.shape:
+        i = matching.index(dim)
+        new_shape[i] = dim
+        matching[i] = None
+    return np.broadcast_to(arr.reshape(new_shape), shape)
+
+
+def _broadcast_labels(labels, ds, feature_dims=[]):
+    shape = _get_data_shape(ds, feature_dims=feature_dims)
+    if isinstance(labels, np.ndarray):
+        # Broadcast for np.ndarray
+        return _broadcast_array(labels, shape)
+
+    elif isinstance(labels, xr.DataArray):
+        # Broadcast for xarray.DataArray
+        data_dims = _get_data_dims(ds, feature_dims=feature_dims)
+        # Determine dimensions to be broadast:
+        bc_dims = set(data_dims) - set(labels.dims) - \
+            set(feature_dims)
+        for dim in bc_dims:
+            labels = xr.concat([labels] * ds.sizes[dim], dim=dim)
+            labels.coords[dim] = ds.coords[dim]
+        labels = labels.transpose(*data_dims)
+        return labels
 
 
 class Classifier:
@@ -169,6 +209,12 @@ class Classifier:
         Must provide methods ``fit`` and ``predict``.
     feature_dims : list, optional
         A list of additional dimensions to use as features.
+        For example, if the dataset has a 'time' dimension and 'time' is in
+        ``feature_dims``, every time step will be treated as an independent
+        variable for classification purposes.
+        Otherwise, all time steps will be treated as additional data
+        dimensions just like 'x' and 'y'.
+
     """
 
     def __init__(self, clf, feature_dims=[]):
@@ -184,15 +230,31 @@ class Classifier:
         labels : xarray.DataArray
             The class labels to train the classifier.
         """
+        if isinstance(labels, xr.Dataset):
+            raise ValueError("`labels` should be an xarray.DataArray or "
+                             "numpy array of the same dimensions "
+                             "as the dataset.")
+        elif isinstance(labels, (xr.DataArray, np.ndarray)):
+            # Squeeze extra dimensions
+            labels = labels.squeeze()
+
+        # Broadcast labels to match data dimensions
+        shape = _get_data_shape(ds, feature_dims=self.feature_dims)
+        if shape != labels.shape:
+            labels = _broadcast_labels(
+                labels, ds, feature_dims=self.feature_dims)
+
         # Ignore labels that are NaN or 0
-        ymask = ~np.isnan(labels)
+        ymask = ~np.isnan(np.array(labels))
         np.greater(labels, 0, out=ymask, where=ymask)
         ymask = ymask.reshape(-1)
+
         # Ignore values of ds that contain NaN
         X = _build_X(ds, feature_dims=self.feature_dims)[ymask]
         y = np.array(labels).reshape(-1)[ymask]
         Xmask = ~np.isnan(X).any(axis=1)
         self.clf.fit(X[Xmask], y[Xmask])
+        return self
 
     def predict(self, ds, func='predict'):
         """
@@ -217,7 +279,14 @@ class Classifier:
         mask = ~np.isnan(X).any(axis=1)
         labels_flat = np.empty(mask.shape) * np.nan
         labels_flat[mask] = self.clf.__getattribute__(func)(X[mask])
-        shape = (nrows(ds), ncols(ds))
-        labels = xr.DataArray(labels_flat.reshape(shape), dims=('y', 'x'),
-                              coords=ds.coords)
+        data_dims = _get_data_dims(ds, feature_dims=self.feature_dims)
+        data_shape = _get_data_shape(ds, feature_dims=self.feature_dims)
+        data_coords = {dim: c for dim, c in ds.coords.items()
+                       if dim in data_dims}
+        labels = xr.DataArray(labels_flat.reshape(data_shape),
+                              dims=data_dims, coords=data_coords)
         return labels
+
+    def fit_predict(self, ds, labels):
+        self.fit(ds, labels)
+        return self.predict(ds)

nd/tests/test_classifier.py
@@ -1,37 +1,120 @@
 import pytest
 import numpy as np
-from nd.classify import Classifier
+import xarray as xr
+from nd import utils
+from nd import classify
 from nd.testing import generate_test_dataset
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
 from numpy.testing import assert_equal
+from xarray.testing import assert_equal as xr_assert_equal
+
+
+def create_mock_classes(dims):
+    shape = (dims['y'], dims['x'])
+    ds = generate_test_dataset(
+        dims=dims,
+        mean=[1, 0, 0, 1], sigma=0.1)
+    ds2 = generate_test_dataset(
+        dims=dims,
+        mean=[10, 0, 0, 10], sigma=0.1)
+    mask = np.zeros(shape, dtype=bool)
+    mask = xr.DataArray(np.zeros(shape, dtype=bool),
+                        dims=('y', 'x'),
+                        coords=dict(y=ds.y, x=ds.x))
+    # Make half of the data belong to each class
+    mask[:, :dims['x']//2] = True
+    ds = ds.where(mask, ds2)
+    labels_true = (mask * 2).where(mask, 1)
+    return ds, labels_true
 
 
 @pytest.mark.parametrize('clf', [
     GaussianNB(),
-    KNeighborsClassifier(3)
+    KNeighborsClassifier(3),
+    RandomForestClassifier(n_estimators=20),
 ])
 def test_classifier(clf):
-    ny, nx = 50, 50
-    ds = generate_test_dataset(
-        dims={'y': ny, 'x': nx},
-        mean=[1, 0, 0, 1], sigma=0.1)
-    ds2 = generate_test_dataset(
-        dims={'y': ny, 'x': nx},
-        mean=[10, 0, 0, 10], sigma=0.1)
-    mask = np.zeros((ny, nx), dtype=bool)
-    mask[15:35, 15:35] = True
-    ds = ds.where(mask, ds2)
-    labels_true = np.ones((ny, nx))
-    labels_true[mask] = 2
+    dims = dict(y=50, x=50)
+    ds, labels_true = create_mock_classes(dims)
 
     # Select 10% for training
     labels_train = labels_true.copy()
-    mask_train = (np.random.rand(ny, nx) < 0.9)
-    labels_train[mask_train] = np.nan
-    c = Classifier(clf)
+    mask_train = (np.random.rand(dims['y'], dims['x']) < 0.1)
+    labels_train = labels_train.where(mask_train)
+    c = classify.Classifier(clf)
     c.fit(ds, labels_train)
-    labels_predicted = c.predict(ds).values
+    labels_predicted = c.predict(ds)
 
     # Expect 100% accuracy for this trivial classification task.
-    assert_equal(labels_predicted, labels_true)
+    xr_assert_equal(labels_predicted, labels_true)
+
+
+@pytest.mark.parametrize('dims', [
+    dict(y=50, x=50, time=10),
+    dict(y=30, x=20, time=5)
+])
+@pytest.mark.parametrize('feature_dims', [
+    [], ['time']
+])
+def test_broadcast(dims, feature_dims):
+    ds, labels = create_mock_classes(dims)
+
+    expected_shape = classify._get_data_shape(ds, feature_dims=feature_dims)
+
+    # Check broadcast for numpy array
+    blabels = classify._broadcast_labels(labels.values, ds, feature_dims)
+    assert blabels.shape == expected_shape
+
+    # Check broadcast for DataArray
+    blabels = classify._broadcast_labels(labels, ds, feature_dims)
+    assert blabels.shape == expected_shape
+
+    # Check values equal along broadcast dimensions
+    bc_dims = set(dims) - set(labels.dims) - set(feature_dims)
+    for d in bc_dims:
+        assert (blabels.std(d) == 0).all()
+
+
+@pytest.mark.parametrize('feature_dims', [
+    [], ['time']
+])
+def test_build_X(feature_dims):
+    dims = dict(y=50, x=50, time=10)
+    ds, labels = create_mock_classes(dims)
+    X = classify._build_X(ds, feature_dims=feature_dims)
+    nrows = np.prod([N for d, N in dims.items() if d not in feature_dims])
+    ncols = len(ds.data_vars) * \
+        np.prod([N for d, N in dims.items() if d in feature_dims])
+    assert X.shape == (nrows, ncols)
+
+
+@pytest.mark.parametrize('feature_dims', [
+    [], ['time']
+])
+@pytest.mark.parametrize('dims', [
+    dict(y=50, x=50, time=10),
+    dict(y=30, x=20, time=5)
+])
+def test_classifier_feature_dims(dims, feature_dims):
+    ds, labels = create_mock_classes(dims)
+    c = classify.Classifier(RandomForestClassifier(n_estimators=20),
+                            feature_dims=feature_dims)
+
+    # Expect 100% accuracy for this trivial classification task.
+    pred = c.fit(ds, labels).predict(ds)
+    xr_assert_equal(
+        pred, classify._broadcast_labels(labels, ds, feature_dims=feature_dims)
+    )
+
+    # Check that the results are the same whether labels
+    # are passed as xr.DataArray or np.ndarray
+    pred_np = c.fit(ds, labels.values).predict(ds)
+    xr_assert_equal(pred, pred_np)
+
+    # Check that prediction result has correct dimensions
+    assert_equal(
+        utils.get_dims(pred),
+        classify._get_data_dims(ds, feature_dims=feature_dims)
+    )